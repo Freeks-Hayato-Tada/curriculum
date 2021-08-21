3571c71c0ba5ba10f4ff337808fc96c392300d3d
Freeks-Hayato-Tada <88979303+Freeks-Hayato-Tada@users.noreply.github.com>
2021-08-21 17:00:40 +0900

    Task1_1.java

--- JavaStudy/Example.java
+++ JavaStudy/Example.java
@@ -1,5 +1,12 @@
 public class Example{
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int number = 123;
+        System.out.println(number);
+
+        String msg = "javaはプログラム言語";
+        System.out.println(msg);
+
     }
 }
